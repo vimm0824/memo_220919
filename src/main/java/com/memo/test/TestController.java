a12d2fd30c51da235a6f54282b1cafb570fefd51
test <test@test.com>
2023-01-12 18:06:11 +0900

    230112 초기환경세팅

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,46 @@
+package com.memo.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.memo.post.dao.PostDAO;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	private PostDAO postDAO;
+
+	@GetMapping("/test1")
+	@ResponseBody
+	public String test1() {
+		return "Hello world!!!";
+	}
+	
+	@GetMapping("/test2")
+	@ResponseBody
+	public Map<String, Object> test2() {
+		Map<String, Object> result = new HashMap<>();
+		result.put("키1", 111);
+		result.put("키2", 222);
+		result.put("키3", 333);
+		return result;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+	
+	@GetMapping("/test4")
+	@ResponseBody
+	public List<Map<String, Object>> test4() {
+		return postDAO.selectPostList();
+	}
+}
